7abf07f829c545a3c15b176b43a86b8ac3e39833
Yaswanth <yaswanthsai2120@gmail.com>
2025-04-17 11:27:10 -0500

    Added props to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
-function Header(){
+function Header({title}){
     return(
         <header className="bg-blue-600 text-white p-4 text-xl text-center">
-            Welcome to React Mastery
+            {title}
         </header>
     );
 }
